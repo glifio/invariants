4d8b0c26b332a78d88946e735fc889ae66271960
Jim Pick <jim@jimpick.com>
2024-03-18 11:07:14 -0700

    Add singletons for SDK and Lotus

--- metrics_test.go
+++ metrics_test.go
@@ -1,10 +1,29 @@
 package invariants
 
 import (
+	"context"
+	"log"
+	"os"
+	"strconv"
 	"testing"
 
+	"github.com/glifio/invariants/singleton"
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	chainID, err := strconv.Atoi(os.Getenv("CHAIN_ID"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	singleton.InitPoolsSDK(
+		context.Background(),
+		int64(chainID),
+		os.Getenv("LOTUS_PRIVATE_ADDR"),
+		os.Getenv("LOTUS_PRIVATE_TOKEN"),
+	)
+}
 
 // TestMetrics calls the REST API, and compares against on-chain
 func TestMetrics(t *testing.T) {
---
+++ singleton/lotus.go
@@ -0,0 +1,130 @@
+package singleton
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/filecoin-project/go-jsonrpc"
+	lotusapi "github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/build"
+)
+
+var lotusAPIOnce sync.Once
+var lotusArchiveAPIOnce sync.Once
+
+type ChainOptions struct {
+	DialAddr string
+	Token    string
+}
+
+type LotusNode struct {
+	Api    lotusapi.FullNodeStruct
+	Closer jsonrpc.ClientCloser
+}
+
+var lotusClient *LotusNode
+var lotusArchiveClient *LotusNode
+
+func ConnectLotus(opts ChainOptions) error {
+	var connectionErr error
+	lotusAPIOnce.Do(func() {
+		log.Printf("new lotus client: %s\n", opts.DialAddr)
+		lotusClient = &LotusNode{}
+		head := http.Header{}
+
+		if opts.Token != "" {
+			head.Set("Authorization", "Bearer "+opts.Token)
+		}
+
+		closer, err := jsonrpc.NewMergeClient(
+			context.Background(),
+			opts.DialAddr,
+			"Filecoin",
+			lotusapi.GetInternalStructs(&lotusClient.Api),
+			head,
+		)
+
+		if err != nil {
+			connectionErr = err
+		}
+
+		chainId, err := lotusClient.Api.EthChainId(context.Background())
+		if err != nil {
+			// default to mainnet
+			chainId = 314
+		}
+		log.Printf("connected to chain id: %v\n", chainId)
+
+		if chainId != 314 {
+			err = build.UseNetworkBundle("calibrationnet")
+			log.Printf("use network bundle: %v\n", "calibrationnet")
+			if err != nil {
+				log.Printf("use network bundle error: %v\n", err)
+				panic(err)
+			}
+		}
+		lotusClient.Closer = closer
+	})
+
+	return connectionErr
+}
+
+func ConnectArchiveLotus(opts ChainOptions) error {
+	var connectionErr error
+	lotusArchiveAPIOnce.Do(func() {
+		log.Printf("new lotus archive client: %s\n", opts.DialAddr)
+		lotusArchiveClient = &LotusNode{}
+		head := http.Header{}
+
+		if opts.Token != "" {
+			head.Set("Authorization", "Bearer "+opts.Token)
+		}
+
+		closer, err := jsonrpc.NewMergeClient(
+			context.Background(),
+			opts.DialAddr,
+			"Filecoin",
+			lotusapi.GetInternalStructs(&lotusClient.Api),
+			head,
+		)
+
+		if err != nil {
+			connectionErr = err
+		}
+
+		chainId, err := lotusClient.Api.EthChainId(context.Background())
+		if err != nil {
+			// default to mainnet
+			chainId = 314
+		}
+		log.Printf("connected to chain id: %v\n", chainId)
+
+		if chainId != 314 {
+			err = build.UseNetworkBundle("calibrationnet")
+			log.Printf("use network bundle: %v\n", "calibrationnet")
+			if err != nil {
+				log.Printf("use network bundle error: %v\n", err)
+				panic(err)
+			}
+		}
+		lotusArchiveClient.Closer = closer
+	})
+
+	return connectionErr
+}
+
+func Lotus() *LotusNode {
+	return lotusClient
+}
+
+func LotusArchive() *LotusNode {
+	return lotusArchiveClient
+}
+
+func (node *LotusNode) Close() {
+	if node.Closer != nil {
+		node.Closer()
+	}
+}
