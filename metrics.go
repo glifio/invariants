747b7b986c2d75ffadc60563bcc1e8c8fc03576c
Jim Pick <jim@jimpick.com>
2024-03-18 10:39:51 -0700

    Add all fields from metrics API response

--- metrics.go
+++ metrics.go
@@ -29,8 +29,20 @@ type MetricsJSON struct {
 }
 
 type MetricsResult struct {
-	Height          uint64
-	PoolTotalAssets *big.Int
+	Height                    uint64
+	Timestamp                 uint64
+	PoolTotalAssets           *big.Int
+	PoolTotalBorrowed         *big.Int
+	PoolTotalBorrowableAssets *big.Int
+	PoolExitReserve           *big.Int
+	TotalAgentCount           uint64
+	TotalMinerCollaterals     *big.Int
+	TotalMinersCount          uint64
+	TotalValueLocked          *big.Int
+	TotalMinersSectors        *big.Int
+	TotalMinerQAP             *big.Int
+	TotalMinerRBP             *big.Int
+	TotalMinerEDR             *big.Int
 }
 
 // GetMetricsFromAPI calls the REST API to get the metrics
@@ -65,10 +77,42 @@ func GetMetricsFromAPI() (*MetricsResult, error) {
 		return nil, fmt.Errorf("failed to decode JSON response: %v", err)
 	}
 
-	fmt.Printf("Jim response %+v\n", response)
+	poolTotalAssets := big.NewInt(0)
+	poolTotalAssets.SetString(response.PoolTotalAssets, 10)
+	poolTotalBorrowed := big.NewInt(0)
+	poolTotalBorrowed.SetString(response.PoolTotalBorrowed, 10)
+	poolTotalBorrowableAssets := big.NewInt(0)
+	poolTotalBorrowableAssets.SetString(response.PoolTotalBorrowableAssets, 10)
+	poolExitReserve := big.NewInt(0)
+	poolExitReserve.SetString(response.PoolExitReserve, 10)
+	totalMinerCollaterals := big.NewInt(0)
+	totalMinerCollaterals.SetString(response.TotalMinerCollaterals, 10)
+	totalValueLocked := big.NewInt(0)
+	totalValueLocked.SetString(response.TotalValueLocked, 10)
+	totalMinersSectors := big.NewInt(0)
+	totalMinersSectors.SetString(response.TotalMinersSectors, 10)
+	totalMinerQAP := big.NewInt(0)
+	totalMinerQAP.SetString(response.TotalMinerQAP, 10)
+	totalMinerRBP := big.NewInt(0)
+	totalMinerRBP.SetString(response.TotalMinerRBP, 10)
+	totalMinerEDR := big.NewInt(0)
+	totalMinerEDR.SetString(response.TotalMinerEDR, 10)
 
 	result := MetricsResult{
-		Height: response.Height,
+		Height:                    response.Height,
+		Timestamp:                 response.Timestamp,
+		PoolTotalAssets:           poolTotalAssets,
+		PoolTotalBorrowed:         poolTotalBorrowed,
+		PoolTotalBorrowableAssets: poolTotalBorrowableAssets,
+		PoolExitReserve:           poolExitReserve,
+		TotalAgentCount:           response.TotalAgentCount,
+		TotalMinerCollaterals:     totalMinerCollaterals,
+		TotalMinersCount:          response.TotalMinersCount,
+		TotalValueLocked:          totalValueLocked,
+		TotalMinersSectors:        totalMinersSectors,
+		TotalMinerQAP:             totalMinerQAP,
+		TotalMinerRBP:             totalMinerRBP,
+		TotalMinerEDR:             totalMinerEDR,
 	}
 	return &result, nil
 }
